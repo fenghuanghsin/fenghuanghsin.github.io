d2aa249f12069c3c09d43e108466bf100705c039
goldshoot0720 <goldshoot0720@gmail.com>
2025-08-22 21:25:52 +0800

    20250822 v5

---
+++ fenghuanghsin.github.io.bat
@@ -0,0 +1,8 @@
+cd "C:\Users\chbon\Documents\fenghuanghsinhugomultithemes\deploy\fenghuanghsin.github.io"
+git init
+git add .
+git commit -m "20250822 v5"
+git remote add origin https://github.com/fenghuanghsin/fenghuanghsin.github.io.git
+git checkout -b main
+git push -u origin main --force
+pause
